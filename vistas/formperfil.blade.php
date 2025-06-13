ba494f972234f985e62eaa77102e59a340fc4a7a
profesor <profesor@dawdistancia.net>
2025-06-13 17:56:13 +0200

    Update formperfil.blade.php

--- vistas/formperfil.blade.php
+++ vistas/formperfil.blade.php
@@ -5,7 +5,7 @@
 {{-- Sección aporta volver a la barra de navegación --}}
 @section('navbar')
 <li class="nav-item">
-    <a class="nav-link" aria-current="page" href="juegoa.php">Volver</a>
+    <a class="nav-link" aria-current="page" href="juego.php">Volver</a>
 </li>
 @endsection
 {{-- Sección muestra el formulario de perfil del usuario --}}
